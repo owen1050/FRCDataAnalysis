b09baeb7656b831ed02a834a85d83ec99bdf314c
Owen Busler <owen1050@gmail.com>
2024-12-18 22:02:27 -0500

    Update DownloadTBAAllDistricts.py

--- src/DownloadTBAAllDistricts.py
+++ src/DownloadTBAAllDistricts.py
@@ -13,7 +13,12 @@ dists = tm.getDistricts()
 for dist in dists:
 	distName = dist['abbreviation']
 	print(distName)
-	
+	teams = tm.getTeamsInDist(dist['abbreviation'])
+	thisDistTeams = []
+	for team in teams:
+		thisDistTeams.append(team['team_number'])
+
+	outputData[distName] = thisDistTeams
 
 file = open(fileName, 'wb+')
 pickle.dump(outputData, file)
